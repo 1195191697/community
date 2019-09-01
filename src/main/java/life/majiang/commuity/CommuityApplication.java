4b570f69d9f2fe1905b54d815f6390fba9cc67b4
1195191697 <1195191697@qq.com>
2019-09-01 16:01:44 +0800

    init

--- src/main/java/life/majiang/commuity/CommuityApplication.java
+++ src/main/java/life/majiang/commuity/CommuityApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CommuityApplication {
+public class  CommuityApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CommuityApplication.class, args);
